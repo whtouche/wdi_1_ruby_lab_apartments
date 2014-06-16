bd64533841ab22c86c840bb23bb52dd53768efab
Ben White <whtouche@gmail.com>
2014-06-16 04:26:42 -0400

    Determine credit rating based on credit score

--- lib/tenant.rb
+++ lib/tenant.rb
@@ -1,7 +1,23 @@
 class Tenant
 
+  attr_reader :name, :age, :credit_score
+
   def initialize(name:, age:, credit_score:)
     @name, @age, @credit_score = name, age, credit_score
+  end
+
+  def credit_rating
+    if credit_score > 760
+      "excellent"
+    elsif credit_score >725
+      "great"
+    elsif credit_score > 600
+      "good"
+    elsif credit_score > 560
+      "mediocre"
+    else
+      "bad"
+    end
   end
 
 end
@@ -9,6 +25,7 @@ end
 =begin
 ### `Tenant`
 has a name, age, and credit score
+
 #B-Instance variables
 has a credit rating, calculated from the credit score as follows:
 #B-Method that returns string based on credit score variable
